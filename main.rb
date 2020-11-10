4369eeff93f62eb0019f25c544d66717cd4a794e
Joshua Tan <josh.tan.tann@gmail.com>
2020-11-09 23:35:09 -0800

    Add in actual game messages
    Signed-off-by: Joshua Tan <josh.tan.tann@gmail.com>

--- main.rb
+++ main.rb
@@ -6,29 +6,48 @@ player1 = Player.new(true)
 player2 = Player.new(false)
 
 while (player1.lives > 0 && player2.lives > 0)
-  question = Question.new
-  puts question.prompt
-  answer = gets.chomp.to_i
+  puts "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3"
+  puts "----- NEW TURN -----"
 
   if player1.turn
+    question = Question.new
+    print "Player 1: "
+    puts question.prompt
+    print "> "
+    answer = gets.chomp.to_i
+
     if answer == question.answer
-      puts "correct. lives remaining: #{player1.lives}"
+      puts "YES! You are correct!"
     else
       player1.lives -= 1
-      puts "wrong. lives remaining: #{player1.lives}"
+      puts "Seriously? No!"
     end
 
     player1.turn = false
     player2.turn = true
   else
+    question = Question.new
+    print "Player 2: "
+    puts question.prompt
+    print "> "
+    answer = gets.chomp.to_i
+
     if answer == question.answer
-      puts "correct. lives remaining: #{player2.lives}"
+      puts "YES! You are correct!"
     else
       player2.lives -= 1
-      puts "wrong. lives remaining: #{player2.lives}"
+      puts "Seriously? No!"
     end
 
     player2.turn = false
     player1.turn = true
   end
 end
+
+if player1.lives == 0
+  puts "Player 2 wins with a score of #{player2.lives}/3"
+else
+  puts "Player 1 wins with a score of #{player1.lives}/3"
+end
+puts "----- GAME OVER -----"
+puts "Good bye!"
